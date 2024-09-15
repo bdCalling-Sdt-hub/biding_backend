36f7e407464538e47cab9a0385fb545bba6b071f
manik <maniksarker652@gmail.com>
2024-09-15 08:55:52 +0600

    manik

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(
   })
 );
 
-// Parser
+// Parser--------------------
 app.use(express.json());
 // app.use(express.json({ limit: '900mb' }));
 app.use(express.urlencoded({ extended: true }));
