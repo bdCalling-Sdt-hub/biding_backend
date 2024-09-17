08bd2a5b3348038c6711c8c3e986e42076973dbb
manik <maniksarker652@gmail.com>
2024-09-17 11:07:21 +0600

    create schema for transaction

--- src/app/modules/payment/payment.model.js
+++ src/app/modules/payment/payment.model.js
@@ -1,58 +1,70 @@
 const { Schema, model } = require("mongoose");
 const ObjectId = Schema.Types.ObjectId;
 
-const paymentSchema = new Schema(
+// const paymentSchema = new Schema(
+//   {
+//     paymentMethod: {
+//       type: String,
+//       enum: ["card"],
+//       required: true,
+//     },
+//     user: {
+//       type: ObjectId,
+//       ref: "User",
+//       required: true,
+//     },
+//     job: {
+//       type: ObjectId,
+//       ref: "Job",
+//       required: true,
+//     },
+//     amount: {
+//       type: Number,
+//       required: true,
+//     },
+//     transactionId: {
+//       type: String,
+//       trim: true,
+//       unique: true,
+//       required: true,
+//     },
+//     note: String,
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
+
+const Payment = model("Payment", paymentSchema);
+
+const transactionSchema = new Schema(
   {
-    paymentMethod: {
+    userName: {
       type: String,
-      enum: ["card"],
       required: true,
     },
-    user: {
-      type: ObjectId,
-      ref: "User",
+    item: {
+      type: String,
       required: true,
     },
-    job: {
-      type: ObjectId,
-      ref: "Job",
-      required: true,
+    paymentStatus: {
+      type: String,
+      default: "paid",
+      enum: ["paid", "unpaid"],
     },
-    amount: {
+    paidAmount: {
       type: Number,
       required: true,
     },
-    transactionId: {
+    paymentType: {
       type: String,
-      trim: true,
-      unique: true,
-      required: true,
+      default: "Online Payment",
     },
-    note: String,
   },
   {
     timestamps: true,
   }
 );
-
-const Payment = model("Payment", paymentSchema);
-
-const transactionSchema = new Schema({
-  email: {
-    type: String,
-    required: true,
-  },
-  amount: {
-    type: Number,
-    required: true,
-  },
-  transactionId: {
-    type: String,
-    trim: true,
-    unique: true,
-    required: true,
-  },
-});
 
 const Transaction = model("Transaction", transactionSchema);
 
