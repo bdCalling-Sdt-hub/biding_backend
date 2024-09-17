f66510130c6d3c7ea9bafccd9b93f17b83437b94
manik <maniksarker652@gmail.com>
2024-09-17 14:51:03 +0600

    make some changes in user model

--- src/app/modules/user/user.model.js
+++ src/app/modules/user/user.model.js
@@ -67,6 +67,14 @@ const UserSchema = new Schema(
       type: Number,
       default: 0,
     },
+    totalWin: {
+      type: Number,
+      default: 0,
+    },
+    availableBid: {
+      type: Number,
+      default: 0,
+    },
     streetAddress: String,
     city: String,
     state: String,
