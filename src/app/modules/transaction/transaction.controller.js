c55fd3965b9df1b64ca544328ad8ca1acd75e84a
manik <maniksarker652@gmail.com>
2024-09-19 15:30:10 +0600

    create controller for transactiong

---
+++ src/app/modules/transaction/transaction.controller.js
@@ -0,0 +1,18 @@
+const transactionService = require("./transaction.service");
+
+const getAllTransaction = catchAsync(async (req, res) => {
+  const result = await transactionService.getAllTransactionFromDB();
+
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Transaction retrieved successfully",
+    data: result,
+  });
+});
+
+const transactionController = {
+  getAllTransaction,
+};
+
+module.exports = transactionController;
