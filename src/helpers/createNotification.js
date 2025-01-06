26a532d384288b5b0bbfc0006d40271d6d232edc
manik <maniksarker652@gmail.com>
2025-01-06 15:36:54 +0600

    make changes in notification create helper function

--- src/helpers/createNotification.js
+++ src/helpers/createNotification.js
@@ -1,10 +1,6 @@
 const Notification = require("../app/modules/notification/notification.model");
 
 const createNotification = async (notificationData) => {
-  // const result = await Notification.create({
-  //   message: notificationMessage,
-  //   receiver,
-  // });
   const result = await Notification.create(notificationData);
   return result;
 };
